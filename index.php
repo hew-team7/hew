a9d319c62557988ef6c522aadc92f976628df549
odagi11 <58579165+odagi11@users.noreply.github.com>
2020-02-17 13:55:24 +0900

    indexの変更

--- index.php
+++ index.php
@@ -19,14 +19,18 @@
 
 <div id="main">
   <div id="left">
-    <p>店舗側はこちら</p>
-    <a href="s_login.php">ログイン</a>
-    <a href="s_reg_in.php">新規登録</a>
+    <div id="left1">
+      <p>店舗側はこちら</p>
+      <button type="button" onclick="location.href='b_login.php'" class="button">ログイン</button>
+      <button type="button" onclick="location.href='b_reg_in.php'">新規登録</button>
+    </div>
   </div>
   <div id="right">
- 　 <p>購入者側はこちら</p>
-    <a href="b_login.php">ログイン</a>
-    <a href="b_reg_in.php">新規登録</a>
+  　 <div id="right1">
+      <p>購入者側はこちら</p>
+      <button type="button" onclick="location.href='s_login.php'" class="button">ログイン</button>
+      <button type="button" onclick="location.href='s_reg_in.php'">新規登録</button>
+    </div>
   </div>
 </div>
 
