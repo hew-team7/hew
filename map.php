206859313e0fade6f27fa9684eedf95695ecf60d
odagi11 <58579165+odagi11@users.noreply.github.com>
2020-02-24 02:08:22 +0900

    map.php 新規作成

--- map.php
+++ map.php
@@ -4,57 +4,41 @@
 require_once 'config.php';
 $cn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB);
 mysqli_set_charset($cn,'utf8');
-$sql = "SELECT id,name,address1,address2 
-FROM shop_list;";
+$sql = "SELECT id,name,address1,address2 FROM shop_list;";
 
 $result = mysqli_query($cn,$sql);
-$row = mysqli_fetch_assoc($result);
+$table_array = array();  // テーブル情報を格納する変数
+while($row = $result->fetch_assoc() ){
+  $table_array[] = $row;
+}
 mysqli_close($cn);
+$cnt = count($table_array);
+
  
 mb_language("Japanese");//文字コードの設定
 mb_internal_encoding("UTF-8");
  
 $apikey = "dj00aiZpPUpiQlVZd3ZoaGc5MiZzPWNvbnN1bWVyc2VjcmV0Jng9NGU-";
 
-//住所1を入れて緯度経度を求める。
-$address = $row['address1'] . $row['address2'];
-$address = urlencode($address);
-$url = "https://map.yahooapis.jp/geocode/V1/geoCoder?output=json&recursive=true&appid=" . $apikey . "&query=" . $address ;
+for ($i=0; $i<$cnt; $i++){
+  //住所1を入れて緯度経度を求める。
+$address[$i] = $table_array[$i]['address1'] . $table_array[$i]['address2'];
+$address[$i] = urlencode($address[$i]);
+$url = "https://map.yahooapis.jp/geocode/V1/geoCoder?output=json&recursive=true&appid=" . $apikey . "&query=" . $address[$i] ;
 $contents = file_get_contents($url);
 $contents = json_decode($contents);
 $Coordinates = $contents ->Feature[0]->Geometry->Coordinates;
 $geo = explode(",", $Coordinates);
-$lon1 = $geo[0];
-$lat1 = $geo[1];
-$name = $row['name'];
+$lon[$i] = $geo[0];
+$lat[$i] = $geo[1];
+
+}
 
 
 
-/* 
-//住所3を入れて緯度経度を求める。
-$address3 = "";
-$address = urlencode($address3);
-$url = "https://map.yahooapis.jp/geocode/V1/geoCoder?output=json&recursive=true&appid=" . $apikey . "&query=" . $address ;
-$contents = file_get_contents($url);
-$contents = json_decode($contents);
-$Coordinates = $contents ->Feature[0]->Geometry->Coordinates;
-$geo = explode(",", $Coordinates);
-$lon3 = $geo[0];
-$lat3 = $geo[1];
-*/
 
-/* 
-//住所4を入れて緯度経度を求める。
-$address4 = "";
-$address = urlencode($address4);
-$url = "https://map.yahooapis.jp/geocode/V1/geoCoder?output=json&recursive=true&appid=" . $apikey . "&query=" . $address ;
-$contents = file_get_contents($url);
-$contents = json_decode($contents);
-$Coordinates = $contents ->Feature[0]->Geometry->Coordinates;
-$geo = explode(",", $Coordinates);
-$lon4 = $geo[0];
-$lat4 = $geo[1];
- */ 
+
+
  
  
 ?><DOCTYPE html>
@@ -80,15 +64,15 @@ $lat4 = $geo[1];
             attribution: "<a href='https://www.openstreetmap.org/copyright' target='_blank'>地理院タイル</a>"
           }).addTo(map);
           //ポップアップオブジェクトを作成
-            var popup1 = L.popup().setContent('<a href="">あ</a>');
-            //var popup3 = L.popup().setContent("<?php //echo $address3?>");
-            //var popup4 = L.popup().setContent("<?php //echo $address4?>");
+            //var popup1 = L.popup().setContent('<a href="">あ</a>');
           //マーカーにポップアップを紐付けする。同時にbindTooltipでツールチップも追加
             L.marker(mpoint, { draggable: true, icon: L.spriteIcon('red')}).bindTooltip("現在地").addTo(map);
-            L.marker([<?php echo $lat1?>, <?php echo $lon1?>]).bindPopup(popup1).bindTooltip("<?php echo $name;?>").addTo(map);
-            //L.marker([<?php //echo $lat3?>, <?php //echo $lon3?>]).bindPopup(popup3).bindTooltip("").addTo(map);
-            //L.marker([<?php //echo $lat4?>, <?php //echo $lon4?>]).bindPopup(popup4).bindTooltip("").addTo(map);
-            mapcontainer.style.width = '800px';
+            //L.marker([<?php //echo $lat1?>, <?php //echo $lon1?>]).bindPopup(popup1).bindTooltip("<?php //echo $name;?>").addTo(map);
+            <?php for($i=0; $i<$cnt; $i++){?>
+              var popup<?php echo $i;?> = L.popup().setContent('<?php echo $table_array[$i]['name'];?><br><?php echo $table_array[$i]['address1'].$table_array[$i]['address2'];?><br><a href=".php?<?php echo $table_array[$i]['id'];?>">商品はこちら</a>');
+              L.marker([<?php echo $lat[$i];?>, <?php echo $lon[$i];?>]).bindPopup(popup<?php echo $i; ?>).bindTooltip("<?php echo $table_array[$i]['name'];?>").addTo(map);
+            <?php } ?>
+            mapcontainer.style.width = '100%';
         },
         // error callback
         function(position) {
