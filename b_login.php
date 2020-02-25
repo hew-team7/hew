7317e3bd6af7b7ab5fdcf1e573f14838eea7bfe8
Narwhal-cloud <60862634+Narwhal-cloud@users.noreply.github.com>
2020-02-25 18:55:38 +0900

    update

--- b_login.php
+++ b_login.php
@@ -56,6 +56,12 @@ if(isset($_POST['log'])){
   <meta charset="UTF-8">
   <title>ログイン画面</title>
   <link rel="stylesheet" type="text/css" href="./css/">
+      <!-- BootstrapのCSS読み込み -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
