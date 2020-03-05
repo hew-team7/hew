ba4e87798c35237b791fb45fec1559a7f61567d1
odagi11 <58579165+odagi11@users.noreply.github.com>
2020-03-06 02:18:13 +0900

    a

--- pl_rgt_in.php
+++ pl_rgt_in.php
@@ -286,8 +286,8 @@ function fitCover50($resize, &$w1, &$h1, &$w2, &$h2, &$x, &$y)
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle waves-effect waves-light" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><i class="fa fa-user"></i> <span class="hidden-sm-down">プロフィール</span> </a>
                     <div class="dropdown-menu dropdown-primary dd-right" aria-labelledby="dropdownMenu1" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
-                        <a class="dropdown-item waves-effect waves-light" href="./b_pl.php">プロフィール確認</a>
-                        <a class="dropdown-item waves-effect waves-light" href="./b_pl_stg.php">プロフィール編集</a>
+                        <a class="dropdown-item waves-effect waves-light" href="./s_pl.php">プロフィール確認</a>
+                        <a class="dropdown-item waves-effect waves-light" href="./s_pl_stg.php">プロフィール編集</a>
                     </div>
                 </li>
             </ul>
