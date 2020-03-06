696d865ed7ab2b38e6739fdb69f14224a7cfc0df
yukiii0529 <57751716+yukiii0529@users.noreply.github.com>
2020-03-06 11:22:30 +0900

    Update pl_exh_cl.php

--- pl_exh_cl.php
+++ pl_exh_cl.php
@@ -12,7 +12,6 @@ if (isset($_SESSION['reason'])) { //2重登録対策
 	$id = $row['id'];
 	$id++;
 	$sql = "INSERT INTO shop_sell_product(id,shop_id, product_id, detail, reason, sell_price, price_cut, sales_comission, sell_quantity, expiration_date,  close_date,discount,sell_point) VALUES ('" . $id . "','" . $_SESSION['shop_number'] . "','" . $_GET['pid'] . "','" . $_SESSION['detail'] . "','" . $_SESSION['a_reason'] . "'," . $_SESSION['price'] . "," . $_SESSION['dis_price'] . "," . $_SESSION['sale_com'] . "," . $_SESSION['quantity'] . ",'" . $_SESSION['ex_date'] . "','" . $_SESSION['pub_date'] . "'," . $_SESSION['discount'] . "," . $_SESSION['point'] . ")";
-	var_dump($sql);
 	mysqli_query($cn, $sql);
 	mysqli_close($cn);
 
